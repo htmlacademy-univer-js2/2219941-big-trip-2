9a60cfcbc86934fe120ed42d145af8fb55662773
rabbi7sf1ght <113305087+rabbi7sf1ght@users.noreply.github.com>
2023-06-07 22:20:32 +0500

    Революция или эволюция? (часть 1)

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import FilterView from './view/filter-view.js';
+import MenuView from './view/menu-view.js';
 import TripPresenter from './presenter/presenter.js';
 import PointsModel from './model/points_model.js';
 import {render} from './render.js';
@@ -7,9 +8,9 @@ const siteMainElement = document.querySelector('.page-main');
 const siteHeaderElement = document.querySelector('.page-header');
 const pointsModel = new PointsModel();
 const tripPresenter = new TripPresenter({
-  contentContainer: siteMainElement,
+  contentContainer: siteMainElement.querySelector('.trip-events'),
   pointsModel: pointsModel});
 
+render(new MenuView(), siteHeaderElement.querySelector('.trip-controls__navigation'));
 render(new FilterView(), siteHeaderElement.querySelector('.trip-controls__filters'));
-
 tripPresenter.init();
